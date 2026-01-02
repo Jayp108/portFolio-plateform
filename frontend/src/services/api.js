b5a2001c39c7c4b54750438be965c923ecf6eda6
Jayp108 <rjayprakash502@gmail.com>
2026-01-02 15:39:20 +0530

    fix api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: `$
+  {import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000'}/api`,
+  withCredentials: true,
+  // headers: {
+    // 'Content-Type': 'application/json',
+  // },
 });
 
 api.interceptors.request.use(
